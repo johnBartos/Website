b4290e5f5fa767338eb4450705891e6d9cc81ebe
John Bartos <jbartos@dataonline.com>
2015-03-18 07:38:52 -0400

    Initial add of files into repo

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+  stylus = require('stylus'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var app = express();
+
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware(
+  {
+    src:__dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/partials/:partialPath', function(req, res){
+  console.log(req.params.partialPath);
+  res.render('partials/' + req.params.partialPath)
+});
+
+app.get('*', function(req, res){
+  res.render('index');
+});
+
+var port = 3030;
+app.listen(port);
+console.log('Listening on port ' + port + ' ...');
