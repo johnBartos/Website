e5d55c5f1aa6b104aba95f6c510018b7866a7e72
John Bartos <jbartos7@gmail.com>
2015-03-19 20:58:09 -0400

    Fixed bodyParser deprecated warnings

--- server.js
+++ server.js
@@ -12,12 +12,23 @@ function compile(str, path) {
     return stylus(str).set('filename', path);
 }
 
+router.use(stylus.middleware(
+    {
+        src:__dirname + '/public',
+        compile: compile
+    }
+));
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
 
 app.use(logger('dev'));
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 app.use(stylus.middleware(
     {
         src:__dirname + '/public',
@@ -26,13 +37,6 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 app.use('/', router);
-
-router.use(stylus.middleware(
-    {
-        src:__dirname + '/public',
-        compile: compile
-    }
-));
 
 
 var port = 3030;
