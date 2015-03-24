744364c253e5c1c086c0895bad2c063e468868c5
John Bartos <jbartos@dataonline.com>
2015-03-24 12:00:57 -0400

    Switched from ngroute to ui-router

--- client/app/app.js
+++ client/app/app.js
@@ -1,11 +1,23 @@
+// angular.module('websiteApp', [
+// 'ngResource',
+// 'ngRoute'
+//  ])
+// .config(function($routeProvider, $locationProvider){
+//     $locationProvider.html5Mode({
+//       enabled: true
+//     });
+//     $routeProvider
+//     .when('/', { templateUrl: 'app/main/main.html', controller: 'MainController'});
+// })
+'use strict';
+
 angular.module('websiteApp', [
 'ngResource',
-'ngRoute'
- ])
-.config(function($routeProvider, $locationProvider){
-    $locationProvider.html5Mode({
-      enabled: true
-    });
-    $routeProvider
-    .when('/', { templateUrl: 'app/main/main.html', controller: 'MainController'});
-})
+'ui.router'
+])
+.config(function ($stateProvider, $urlRouterProvider, $locationProvider){
+  $urlRouterProvider
+  .otherwise('/');
+
+  $locationProvider.html5Mode(true);
+});
