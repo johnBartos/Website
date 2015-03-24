15a927154bc90712d6feb69c06d219db90de47ce
John Bartos <jbartos@dataonline.com>
2015-03-24 14:58:18 -0400

    Renamed client/vendor to client/bower_components, added basic CSS style

--- client/app/app.js
+++ client/app/app.js
@@ -1,14 +1,3 @@
-// angular.module('websiteApp', [
-// 'ngResource',
-// 'ngRoute'
-//  ])
-// .config(function($routeProvider, $locationProvider){
-//     $locationProvider.html5Mode({
-//       enabled: true
-//     });
-//     $routeProvider
-//     .when('/', { templateUrl: 'app/main/main.html', controller: 'MainController'});
-// })
 'use strict';
 
 angular.module('websiteApp', [
