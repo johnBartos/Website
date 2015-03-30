50ea2bfc938d8ef7e16f50d9c6904fa132007980
John Bartos <jbartos@dataonline.com>
2015-03-30 15:25:50 -0400

    Added DB call to jobs router, added jobs DB seed file

--- server/api/jobs/jobs.controller.js
+++ server/api/jobs/jobs.controller.js
@@ -1,10 +1,19 @@
 'use strict'
+var db = require('./jobs.database');
+
+// exports.index = function(req, res){
+//     res.json([
+//     {
+//         name: "DataOnline",
+//         info: "Current job"
+//     }
+//     ]);
+// };
 
 exports.index = function(req, res){
-    res.json([
-    {
-        name: "DataOnline",
-        info: "Current job"
-    }
-    ]);
+  // db = req.app.get('db');
+  console.log("Getting records");
+  db.get(function(rec){
+    res.json(rec);
+  });
 };
--- server/routes.js
+++ server/routes.js
@@ -19,6 +19,6 @@ module.exports = function(app)
 
     app.route('/*')
         .get(function(req, res){
-            res.render(app.get('appPath') + '/index.html');
+            res.sendfile(app.get('appPath') + '/index.html');
         });
 };
