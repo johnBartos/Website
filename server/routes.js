74ad2bc0c5326ab75d3ceeb5228ec85aabaee272
John Bartos <jbartos@dataonline.com>
2015-04-07 15:29:00 -0400

    Added smaller logos, recolored panels, fixed? refresh routing issue

--- server/routes.js
+++ server/routes.js
@@ -1,15 +1,4 @@
 'use strict';
-// var compile = function (str, path) {
-//     return stylus(str).set('filename', path);
-// }
-
-// router.use(stylus.middleware(
-//     {
-//         src:__dirname + '/public',
-//         compile: compile
-//     }
-// ));
-
 
 module.exports = function(app)
 {
@@ -21,6 +10,6 @@ module.exports = function(app)
 
     app.route('/*')
         .get(function(req, res){
-            res.sendfile(app.get('appPath') + '/index.html');
+            res.sendFile('index.html', {"root": app.get('appPath')});
         });
 };
