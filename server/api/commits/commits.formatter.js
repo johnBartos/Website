4f6d5764effc5b0a55f8b026935bfa2a7dcd5a0b
John Bartos <jbartos@dataonline.com>
2015-06-18 16:03:07 -0400

    Added momentjs, refactored trunc/css

--- server/api/commits/commits.formatter.js
+++ server/api/commits/commits.formatter.js
@@ -1,3 +1,5 @@
+var moment = require('moment');
+
 exports.format = function (blob, n){
   var commits = JSON.parse(blob);
   var last_commits = commits.slice(0, n);
@@ -9,38 +11,22 @@ exports.format = function (blob, n){
       var formatted_commit = {
         name: record.commit.committer.name,
         email: record.commit.committer.email,
-        date: record.commit.committer.date,
-        message: record.commit.message,
-        html_url: record.html_url,
+        date: moment(record.commit.committer.date).fromNow(),
+        message: record.commit.message.trunc(55),
+        commit_url: record.html_url,
         avatar: record.committer.avatar_url,
-        url: record.committer.url
+        committer_url: record.committer.html_url
       };
       formatted_commits.push(formatted_commit);
     }
     return formatted_commits;
 }
 
+String.prototype.trunc = function(n){
+    if(this.length < n)
+      return this;
 
-// var CommitList = function(blob)
-// {
-//   this.jsonArray = JSON.parse(blob);
-// }
-//
-// Commit.prototype.last(n){
-//   var formatted_commits = [];
-//   var last_commits = jsonArray.slice(0, n);
-//   for(record in last_commits)
-//     {
-//       var formatted_commit = {
-//         name: record.commit.committer.name,
-//         email: record.commit.committer.email,
-//         date: record.commit.committer.date,
-//         message: record.commit.message,
-//         html_url: record.html_url,
-//         avatar: record.committer.avatar_url,
-//         url: record.committer.url
-//       };
-//       formatted_commits.push(formatted_commit);
-//     }
-//   return formatted_commits;
-// }
+    s_ = this.substr(0, n-1);
+    s_ = s_.substr(0, s_.lastIndexOf(' '));
+    return s_ + '...';
+}
