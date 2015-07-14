8d82183e211c2e310da8fed0697042156850f27c
John Bartos <jbartos7@gmail.com>
2015-07-14 09:01:32 -0400

    Created filter to allow HTML in blog posts; removed stale content from jobs page

--- client/app/blog/blog.controller.js
+++ client/app/blog/blog.controller.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('websiteApp')
-.controller('BlogController', function ($scope, $http) {
+.controller('BlogController', function ($scope, $http, $sce) {
   $scope.posts = [];
 
   $http.get('/api/posts').success(function(posts){
     $scope.posts  = posts;
-  });
-});
+  })})
+
+.filter('to_trusted', ['$sce', function($sce){
+  return function(text){
+    return $sce.trustAsHtml(text);
+  };
+}]);
