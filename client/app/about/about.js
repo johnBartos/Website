da79bbf666b3a30e27a80982bb01d0c97c2a618c
John Bartos <jbartos7@gmail.com>
2015-08-21 08:01:13 -0400

    Remove unused about page

--- client/app/about/about.js
+++
@@ -1,9 +0,0 @@
-angular.module('websiteApp')
-.config(function ($stateProvider) {
-  $stateProvider
-  .state('about', {
-    url: '/about',
-    templateUrl: 'app/about/main.html',
-    controller: 'MainController'
-  });
-});
