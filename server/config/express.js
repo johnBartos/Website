7d378286cd7f71e298c6df6c00ae7fccb95b50ca
John Bartos <jbartos7@gmail.com>
2015-09-09 20:23:03 -0400

    Cleanup unused packages

--- server/config/express.js
+++ server/config/express.js
@@ -1,17 +1,13 @@
 'use strict';
 
 var express = require('express');
-var stylus = require('stylus');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var path = require('path');
 var config = require('./environment');
 
 module.exports = function(app){
-
-    //app.set('views', config.root + '/server/views');
     app.set('db', config.root + '/server/database/database.js');
-    //app.set('view engine', 'jade');
     app.use(logger('dev'));
     app.use(bodyParser.urlencoded({
         extended: true
