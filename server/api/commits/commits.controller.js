721ec27e678fcc0d50f61ac5d505933f3ceae7a7
John Bartos <jbartos7@gmail.com>
2015-09-13 14:56:02 -0400

    Cleanup controller

--- server/api/commits/commits.controller.js
+++ server/api/commits/commits.controller.js
@@ -1,58 +1,6 @@
 'use strict'
 var rp = require('request-promise');
 var formatter = require('./commits.formatter.js');
-var transform = require('stream').Transform;
-
-
-var options = {
-  uri: 'https://api.github.com/repos/johnBartos/Website/commits',
-  method: 'GET',
-  headers: {'user-agent': 'node.js'}
-}
-
-exports.index = function(req, res){
-  console.log('Getting commits from Git');
-  request(options, function (error, response, body) {
-    console.log('Response from Git: ' + response.statusCode);
-    if (!error && response.statusCode == 200) {
-      var formattedCommits = formatter.format(body, 5);
-      res.json(formattedCommits);
-    }
-  })
-};
-
-
-function getReposOptions() {
-  return {
-    uri: 'https://api.github.com/users/johnBartos/repos',
-    method: 'GET',
-    headers: {'user-agent': 'node.js'},
-    transform: getReposTransform
-  };
-}
-
-function getCommitsOptions(repo) {
-  return {
-    uri: 'https://api.github.com/repos/johnBartos/' + repo + '/commits',
-    method: 'GET',
-    headers: {'user-agent': 'node.js'},
-    transform: getCommitsTransform
-  };
-}
-
-var getReposTransform = function (userManifest) {
-    var repoNames = [];
-    var repos = JSON.parse(userManifest);
-    repos.forEach( function (element, index, array) {
-      var repo = element;
-      repoNames.push(repo.name);
-    });
-    return repoNames;
-};
-
-var getCommitsTransform = function (allCommits) {
-  return formatter.format(allCommits, 5);
-};
 
 exports.getRepos = function (req, res) {
   console.log('Getting repos');
@@ -98,3 +46,37 @@ exports.getCommits = function (req, res) {
 
   getCommits(repoName);
 };
+
+
+var getReposTransform = function (userManifest) {
+    var repoNames = [];
+    var repos = JSON.parse(userManifest);
+    repos.forEach( function (element, index, array) {
+      var repo = element;
+      repoNames.push(repo.name);
+    });
+    return repoNames;
+};
+
+var getCommitsTransform = function (allCommits) {
+  return formatter.format(allCommits, 5);
+};
+
+
+function getReposOptions() {
+  return {
+    uri: 'https://api.github.com/users/johnBartos/repos',
+    method: 'GET',
+    headers: {'user-agent': 'node.js'},
+    transform: getReposTransform
+  };
+}
+
+function getCommitsOptions(repo) {
+  return {
+    uri: 'https://api.github.com/repos/johnBartos/' + repo + '/commits',
+    method: 'GET',
+    headers: {'user-agent': 'node.js'},
+    transform: getCommitsTransform
+  };
+}
