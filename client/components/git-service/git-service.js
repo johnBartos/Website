489edda4e8c03931449f81329abefa0a491b90ed
John Bartos <jbartos7@gmail.com>
2015-09-13 22:15:38 -0400

    Fleshing out git-service

--- client/components/git-service/git-service.js
+++ client/components/git-service/git-service.js
@@ -1,4 +1,21 @@
-angular.module('website')
-.service('gitService', function () {
+angular.module('websiteApp')
+.service('gitService', function ($http) {
+
+  this.getCommits = function() {
+
+    $http.get('/api/commits/repos')
+    .then(function(result) {
+      console.log(result);
+      var repos = result.data;
+      return $http.get('/api/commits/repo/' + repos[0]);
+    })
+    .then(function(result) {
+      console.log(result);
+      var commits = result.data;
+    })
+    .catch(function (error) {
+      console.log('ERROR ' + error);
+    });
+  }
 
 });
