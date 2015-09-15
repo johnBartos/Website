6297faeeb2b7e4b831d78fc7b94a220e92dceeb7
John Bartos <jbartos7@gmail.com>
2015-09-14 22:22:58 -0400

    Get karma running with an easy test

--- client/components/git-service/git-service.js
+++ client/components/git-service/git-service.js
@@ -1,31 +1,22 @@
 angular.module('websiteApp')
 .service('gitService', function ($http) {
 
-  this.getCommits = function() {
+  this.getRepos = function () {
 
     $http.get('/api/commits/repos')
-    .then(function(result) {
+    .then(function (result) {
       console.log(result);
       var repos = result.data;
-      return function() {
-      repos.forEach(function (element, index, array) {
-        getCommitForRepo(element);
-      });
-    }
-    })
-    .then(function(result) {
-      console.log(result);
-      //var commits = result.data;
+      return repos;
     })
     .catch(function (error) {
       console.log('ERROR ' + error);
     });
+
   };
 
-  function getCommitForRepo(repo) {
+  function getCommitForRepo (repo) {
     return $http.get('/api/commits/repo/' + repo);
   }
-
-
 
 });
