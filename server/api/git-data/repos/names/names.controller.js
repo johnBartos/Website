6340c4c25b1bfef3a8f1e19a0c3b1647edff5c51
John Bartos <jbartos7@gmail.com>
2015-09-21 21:02:31 -0400

    Scaffold redis caching with for getting names

--- server/api/git-data/repos/names/names.controller.js
+++ server/api/git-data/repos/names/names.controller.js
@@ -1,27 +1,40 @@
 'use strict'
 var rp = require('request-promise');
+var errors = require('request-promise/errors');
+var redis = require('redis');
 
 exports.getRepoNames = function (req, res) {
   console.log('Getting repos');
 
   var getRepos = function () {
-    var options = getReposOptions();
 
+    var options = getReposOptions();
     return rp.get(options)
-    .then(function(repoNames) {
+    .then(function(repoNames, response) {
+      console.log('response is ' + response);
       res.status(200).json(repoNames);
-      })
-    .catch(function (reason) {
+    })
+    .catch(errors.StatusCodeError, function (reason) {
       console.log(reason);
-      res.status(400).json({
+      if(reason.status == '304') {
+
+      }
+      else {
+        res.status(400).json({
+          success: false,
+          reason: reason
+        });
+      }
+    })
+    .catch(errors.RequestError,function (reason) {
+      res.status(500).json({
         success: false,
         reason: reason
       });
     });
 
   };
-
-  getRepos()
+  getRepos();
 };
 
 var getReposTransform = function (userManifest) {
@@ -42,4 +55,21 @@ function getReposOptions() {
     headers: {'user-agent': 'node.js'},
     transform: getReposTransform
   };
+}
+
+function getRepoNamesFromCache(eTag) {
+  return new Promise (function (resolve, reject) {
+    try {
+      var client = redis.createClient();
+    }
+    catch (exception) {
+      reject();
+    }
+  });
+}
+
+function saveRepoNamesToCache() {
+  return new Promise ( function(resolve, reject) {
+
+  });
 }
