6583f352c82d7b35deddbdcaa26d403b805f2af6
John Bartos <jbartos7@gmail.com>
2015-09-24 20:18:58 -0400

    Refactor mongo call to return promise to controller

--- server/api/jobs/technologies/technologies.controller.js
+++ server/api/jobs/technologies/technologies.controller.js
@@ -4,7 +4,11 @@ var db = require('./technologies.database');
 
 exports.get = function (req, res){
   console.log("Getting Technologies: " + req.params.jobId);
-  db.get(req.params.jobId, function(rec){
-    res.json(rec);
+  db.get(req.params.jobId)
+  .then(function (technologies) {
+    res.json(technologies);
+  })
+  .catch(function (reason) {
+    res.status(500).json(reason);
   });
 };
