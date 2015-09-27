27a228130bca33c385578048d9617b9b30e57ba8
John Bartos <jbartos7@gmail.com>
2015-09-27 18:44:19 -0400

    Scaffold new database

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 var config = require('./config/environment');
 var app = express();
+
 require('./config/express')(app);
 require('./routes.js')(app);
 
---
+++ server/database/database.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var mongoClient = require('mongod');
+var url = "mongodb://localhost:27017/LocalDB";
+
+var connection = {};
+
+exports.connect = function () {
+
+};
+
+exports.get = function () {
+
+};
