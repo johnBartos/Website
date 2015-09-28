8424cd8660d624969b5a593e38da45ce1900e7be
John Bartos <jbartos7@gmail.com>
2015-09-27 22:50:58 -0400

    Create database module to prevent mongo connections from being opened each page view

--- server/api/projects/projects.controller.js
+++ server/api/projects/projects.controller.js
@@ -1,12 +1,13 @@
 'use strict';
 
-var db = require('./projects.database');
+// var db = require('./projects.database');
+var db = require('../../database/database.js');
 
 exports.index = function (req, res){
 
   console.log("Getting Projects");
 
-  db.get()
+  db.getFromCollection('Projects')
   .then(function (projects) {
     res.json(projects);
   })
