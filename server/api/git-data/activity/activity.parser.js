c45b4e7c39c176a64f73c03e2635dde0e70c538b
John Bartos <jbartos7@gmail.com>
2015-09-30 21:00:24 -0400

    Activity parser now returns properly formatted commit, updated jshint

--- server/api/git-data/activity/activity.parser.js
+++ server/api/git-data/activity/activity.parser.js
@@ -6,25 +6,38 @@ var parser = module.exports = {};
 
 parser.parse = function (activityList) {
   var commits = getCommits(JSON.parse(activityList));
+
   console.log(commits);
-  return flattenCommits(commits);
+
+  return parseCommits(commits);
 };
 
 function getCommits (activityJson) {
-  return activityJson.filter(commitsOnly);
+  return activityJson.filter(function (activity) {
+     return activity.type == 'PushEvent';
+   });
 }
 
-function commitsOnly (activity) {
-  return activity.type == 'PushEvent';
-}
+function parseCommits (pushList) {
+  var parsedCommits = [];
 
-function flattenCommits (commitList) {
-  return commitList.map(function (c) {
-    return {
-      date: c.created_at,
-      avatar_url: c.avatar_url,
-      repo: c.repo,
-      commits: c.payload.commits
+  for (var push of pushList) {
+    var date = push.created_at;
+    var avatar_url = push.avatar_url;
+    var repo = push.repo;
+
+    for (var commit of push.payload.commits) {
+      var formattedCommit = {
+        date: moment(date).fromNow(),
+        avatar_url: avatar_url,
+        url: commit.url,
+        message: commit.message,
+        repo: repo
     };
-  });
+
+    parsedCommits.push(formattedCommit);
+  }
+}
+
+  return parsedCommits;
 }
