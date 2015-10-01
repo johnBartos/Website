fe16505231465874a7d5f3bde728830b7f513529
John Bartos <jbartos7@gmail.com>
2015-09-30 21:18:08 -0400

    Update git-service to return values from new activity endpoint

--- client/components/git-service/git-service.js
+++ client/components/git-service/git-service.js
@@ -1,17 +1,32 @@
 (function () { 'use strict'; })();
 
 angular.module('websiteApp')
-.service('gitService', function ($http, $q) {
+  .service('gitService', function ($http, $q) {
 
   this.getActivity = function () {
-    return getRepos()
-    .then(getCommits);
+    return getActivity();
   };
 
+  this.getCommits = function () {
+    return getRepos()
+    .then(getCommitsForAllRepos);
+  };
+
+  function getActivity () {
+    return new Promise(function (resolve, reject) {
+      $http.get('/api/git-data/activity')
+        .then(function (result) {
+          resolve(result.data);
+        })
+        .catch(function (error) {
+          console.log('ERROR ' + error);
+          reject();
+        });
+    });
+  }
+
   function getRepos () {
-
-    return new Promise( function (resolve, reject) {
-
+    return new Promise(function (resolve, reject) {
       $http.get('/api/git-data/repos/names')
         .then(function (result) {
           resolve(result.data);
@@ -20,37 +35,32 @@ angular.module('websiteApp')
           console.log('ERROR ' + error);
           reject();
         });
-
-  });
-
+      });
   }
 
-  function getCommits (repoNames) {
-
+  function getCommitsForAllRepos (repoNames) {
     var allCalls = [];
 
     repoNames.forEach(function (element, index, array) {
         allCalls.push($http.get('/api/git-data/repos/' + element + '/commits'));
-  });
+    });
 
-  return new Promise (function (resolve, reject) {
-
-    $q.all(allCalls)
-      .then(function (result) {
-        var listOfCommits = result.map(function (c) {
-          return c.data;
+    return new Promise (function (resolve, reject) {
+      $q.all(allCalls)
+        .then(function (result) {
+          var listOfCommits = result.map(function (c) {
+            return c.data;
+          })
+          .reduce(function (a, b) {
+            return a.concat(b);
+          });
+          resolve(listOfCommits);
         })
-        .reduce(function (a, b) {
-          return a.concat(b);
+        .catch(function (error) {
+          console.log('ERROR ' + error);
+          reject();
         });
-        resolve(listOfCommits);
-      })
-      .catch(function (error) {
-        console.log('ERROR ' + error);
-        reject();
-      });
-  });
-
+    });
   }
 
 });
