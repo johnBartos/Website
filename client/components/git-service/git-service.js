508b5e5584dadb498dc8d1995c43f370a72ca4d7
John Bartos <jbartos7@gmail.com>
2015-10-17 00:19:10 -0400

    Remove unused function from gitService

--- client/components/git-service/git-service.js
+++ client/components/git-service/git-service.js
@@ -7,60 +7,16 @@ angular.module('websiteApp')
     return getActivity();
   };
 
-  this.getCommits = function () {
-    return getRepos()
-    .then(getCommitsForAllRepos);
-  };
-
   function getActivity () {
     return new Promise(function (resolve, reject) {
       $http.get('/api/git-data/activity')
         .then(function (result) {
           resolve(result.data);
         })
-        .catch(function (error) {
-          console.log('ERROR ' + error);
-          reject();
+        .catch(function (reason) {
+          console.log('ERROR ' + reason);
+          reject(reason);
         });
     });
   }
-
-  function getRepos () {
-    return new Promise(function (resolve, reject) {
-      $http.get('/api/git-data/repos/names')
-        .then(function (result) {
-          resolve(result.data);
-        })
-        .catch(function (error) {
-          console.log('ERROR ' + error);
-          reject();
-        });
-      });
-  }
-
-  function getCommitsForAllRepos (repoNames) {
-    var allCalls = [];
-
-    repoNames.forEach(function (element, index, array) {
-        allCalls.push($http.get('/api/git-data/repos/' + element + '/commits'));
-    });
-
-    return new Promise (function (resolve, reject) {
-      $q.all(allCalls)
-        .then(function (result) {
-          var listOfCommits = result.map(function (c) {
-            return c.data;
-          })
-          .reduce(function (a, b) {
-            return a.concat(b);
-          });
-          resolve(listOfCommits);
-        })
-        .catch(function (error) {
-          console.log('ERROR ' + error);
-          reject();
-        });
-    });
-  }
-
 });
