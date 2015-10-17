0fc6d4cd28b64a0bf5186dae2ec9d8e2ca586bb1
John Bartos <jbartos7@gmail.com>
2015-10-16 22:31:44 -0400

    Implement git-service using $q.all to call all endpoints at once

--- client/app/experience/jobs/jobs.controller.js
+++ client/app/experience/jobs/jobs.controller.js
@@ -1,69 +1,33 @@
 (function () { 'use strict'; })();
 
 angular.module('websiteApp')
-.controller('JobsController', function ($scope, $http, $stateParams) {
+.controller('JobsController', function ($scope, $http, $stateParams, jobService) {
 
-  $scope.Job = {};
-  $scope.Job.About = {};
-  $scope.Job.Projects = [];
-  $scope.Job.Languages = [];
-  $scope.Job.Technologies = [];
-  $scope.Job.Works = [];
-
+  $scope.job = {};
 
   activate();
 
   function activate () {
 
-    getJob()
-      .then(getTechnologies)
-      .then(getLanguages)
-      .then(getWorks);
+     jobService.getJob($stateParams.jobId)
+     .then(function (result) {
+       console.log(result);
+       $scope.job = result;
+       console.log($scope.job);
+       $scope.$apply();
+     })
+     .catch(function (reason) {
+       console.log(reason);
+     });
 
 
-     function getJob () {
-      return $http.get('/api/jobs/' + $stateParams.jobId)
-      .then(function (response) {
-        $scope.Job.About = response.data[0];
-      }, function (error) {
-        console.log('error getting job ' + $stateParams.jobId);
-      });
-    }
-
-     function getLanguages () {
-      return $http.get('/api/jobs/languages/' + $stateParams.jobId)
-      .then(function (response) {
-        $scope.Job.Languages = response.data;
-      }, function (error) {
-        console.log('error getting languages for job ' + $stateParams.jobId);
-      });
-    }
-
-    function getWorks () {
-      return $http.get('/api/jobs/works/' + $stateParams.jobId)
-      .then(function (response) {
-        $scope.Job.Works = response.data;
-
-        // Hack for getting Masonry to work
-        setTimeout(function () {
-          $(".masonry-container").masonry({
-              itemSelector: ".item",
-              columnWidth: ".item"
-          });
-        }, 200);
-      }, function (error) {
-        console.log('error getting works for job ' + $stateParams.jobId);
-      });
-    }
-
-    function getTechnologies () {
-      return $http.get('/api/jobs/technologies/' + $stateParams.jobId)
-      .then(function (response) {
-        $scope.Job.Technologies = response.data;
-      }, function (error) {
-        console.log('error getting technologies job ' + $stateParams.jobId);
-      });
-    }
+    // Hack for getting Masonry to work
+    // setTimeout(function () {
+    //   $(".masonry-container").masonry({
+    //       itemSelector: ".item",
+    //       columnWidth: ".item"
+    //   });
+    // }, 200);
 
   }
 
--- client/components/job-service/job-service.js
+++ client/components/job-service/job-service.js
@@ -3,9 +3,66 @@
 angular.module('websiteApp')
   .service('jobService', function ($http, $q) {
 
+    // var Job = {};
+    // Job.About = {};
+    // Job.Projects = [];
+    // Job.Languages = [];
+    // Job.Technologies = [];
+    // Job.Works = [];
+
     this.getJob = function (jobId)
     {
+      var endpoints = [
+        createJobsEndpoint(jobId),
+        createLanguagesEndpoint(jobId),
+        createTechnologiesEndpoint(jobId),
+        createWorksEndpoint(jobId)
+      ];
+
+      return new Promise(function (resolve, reject) {
+        $q.all(endpoints)
+          .then(function (result) {
+            console.log(result);
+              var parsedData = parseResult(result);
+              resolve(parsedData);
+          })
+          .catch(function (reason) {
+            console.log('Error retrieving job data: ' + reason);
+            reject(reason);
+          });
+      });
 
     };
+
+    function parseResult (result)
+    {
+      var about = result[0].data[0];
+      var languages = result[1].data;
+      var tech = result[2].data;
+      var works = result[3].data;
+
+      return {
+        about : about,
+        languages: languages,
+        technologies: tech,
+        works: works
+      };
+    }
+
+    function createJobsEndpoint (jobId) {
+     return $http.get('/api/jobs/' + jobId);
+   }
+
+    function createLanguagesEndpoint (jobId) {
+     return $http.get('/api/jobs/languages/' + jobId);
+   }
+
+   function createTechnologiesEndpoint (jobId) {
+     return $http.get('/api/jobs/technologies/' + jobId);
+   }
+
+   function createWorksEndpoint (jobId) {
+     return $http.get('/api/jobs/works/' + jobId);
+   }
 
   });
