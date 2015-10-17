564c8fb9df5216f9a8cf72550d25240342539ca4
John Bartos <jbartos7@gmail.com>
2015-10-16 23:43:49 -0400

    Cleanup main controller

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,20 +6,18 @@ angular.module('websiteApp')
   $scope.activity.events = [];
   $scope.activity.event = {};
 
-  activate();
+  $scope.$apply(
+    activate()
+  );
 
   function activate () {
-    var start = Date.now();
     gitService.getActivity()
     .then(function (result) {
-      var end = Date.now();
-      console.log('Request time: ' + (end - start) + 'ms');
       $scope.activity.events = result;
       $scope.activity.event = result[0];
-      $scope.$apply();
     })
     .catch (function (reason) {
-      console.log('couldnt get activity from git-service');
+      console.log('Couldnt get activity from git-service');
     });
   }
 
